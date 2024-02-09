6506b23b54c8d5544ed6dd5bea8dade723676351
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-09 10:10:59 +0100

    calculator works as console log app

--- solution.js
+++ solution.js
@@ -1,1 +1,17 @@
-// Write your code here
+
+const cur1Amount = .03;
+const cur1 = 'BTC';
+const cur2 = 'USD';
+const url = `https://api.coinbase.com/v2/prices/${cur1}-${cur2}/spot`;
+
+// then
+fetch(url).then((response) => {
+	// console.log(response);
+	response.json().then((payload) => {
+		const strAmount = payload.data.amount;
+		// const amount = Number(strAmount);
+		const amount = parseFloat(strAmount);
+		const cur2Amount = cur1Amount * amount;
+		console.log(cur2Amount);
+	});
+})
